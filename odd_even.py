ded730b4d38659f2f76a3793b71368736242317f
Lushiness <lucy.gichu@strathmore.edu>
2025-04-25 18:06:05 +0300

    Checking if a number is even or odd.

---
+++ odd_even.py
@@ -0,0 +1,6 @@
+num=input("Enter a number") # Prompts the user to enter a number
+num=int(num) #changes the number to an integer
+if num % 2 == 0: #checks if the number is even or odd
+    print(str(num) + " is even") #changes the int (num) into string and prints if num is even 
+else :
+    print(str(num) + " is odd")#changes the int (num) into string and prints prints if num is odd
