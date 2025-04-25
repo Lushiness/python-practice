2262346e44ebf24c9fa13c768568539b3587a38c
Lushiness <lucy.gichu@strathmore.edu>
2025-04-25 08:35:53 +0300

    the sum of elements in a list

---
+++ sum_elements.py
@@ -0,0 +1,3 @@
+elements=[1,2,3,4,5,6,7,8,9,10] #initializing a list in a variable called elements
+sum_of_elements=sum(elements) #creating a variable sum_of_elements to store the sum of the list declared
+print(sum_of_elements) #outputs the sum
